50f76a37bbec9e5c407184c3cd4153ae5ae7ef62
Samson Kayembe <mzeesam@gmail.com>
2022-06-25 18:38:13 +0300

    Add project files.

---
+++ WebApplication1/CreateUser.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CreateUser.aspx.cs" Inherits="WebApplication1.CreateUser" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>Username:</td>
+                    <td>
+                        <asp:TextBox ID="UserNameTextBox" runat="server"></asp:TextBox>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td>Email:</td>
+                    <td>
+                        <asp:TextBox ID="EmailTextBox" runat="server"></asp:TextBox>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td>Password:</td>
+                    <td>
+                        <asp:TextBox ID="PasswordTextBox" TextMode="Password" runat="server"></asp:TextBox>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <asp:Button ID="btnCreate" runat="server" OnClick="btnCreate_Click" Text="Create User" />
+                    </td>
+                </tr>
+                  <tr>
+                    <td></td>
+                    <td>
+                        <asp:CheckBoxList ID="RolesList" runat="server"></asp:CheckBoxList>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <asp:Label runat="server" ID="lblOutput"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Find User" />
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtRoleName" runat="server"></asp:TextBox>
+                        <asp:Button ID="btnAddRole" runat="server" OnClick="btnAddRole_Click" Text="Add New Role" />
+                    </td>
+                    <td>
+
+                        <asp:Button ID="btnAssignUser" runat="server" OnClick="btnAssignUser_Click" Text="Assign User To Role" />
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/CreateUser.aspx.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class CreateUser : System.Web.UI.Page
+    {
+        static UserStore<IdentityUser> userStore = new UserStore<IdentityUser>();
+        UserManager<IdentityUser> manager = new UserManager<IdentityUser>(userStore);
+
+        static RoleStore<IdentityRole> sStore = new RoleStore<IdentityRole>();
+        static RoleManager<IdentityRole> rManager = new RoleManager<IdentityRole>(sStore);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblOutput.ForeColor = System.Drawing.Color.Black;
+            if (!IsPostBack)
+            {
+
+                LoadRoles();
+            }
+        }
+
+        private void LoadRoles()
+        {
+            var roles = rManager.Roles.ToList();
+            RolesList.Items.Clear();
+            foreach (var role in roles)
+            {
+                var item = new ListItem(role.Name, role.Id);
+                RolesList.Items.Add(item);
+            }
+        }
+
+        protected void btnCreate_Click(object sender, EventArgs e)
+        {
+
+            var user = new IdentityUser
+            {
+                UserName = UserNameTextBox.Text,
+                Email = EmailTextBox.Text
+            };
+
+            var IdentityResult = manager.Create(user, PasswordTextBox.Text);
+            if (IdentityResult.Succeeded)
+            {
+                lblOutput.Text = $"{user.UserName} user has been created.";
+            }
+            else
+            {
+                lblOutput.Text = IdentityResult.Errors.ToList()[0];
+                lblOutput.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var user = manager.FindByEmail(TextBox1.Text);
+
+            if (user != null)
+            {
+                lblOutput.Text = $"User found: {user.UserName}";
+
+                //display roles
+                //foreach (var item in roles)
+                //    {
+                //        lblOutput.Text += item.Name + "<br />";
+                //    }
+            }
+        }
+
+        protected void btnAddRole_Click(object sender, EventArgs e)
+        {
+            var roles = rManager.Roles.ToList();
+            var nRole = new IdentityRole() { Id = (roles.Count() + 1).ToString(), Name = txtRoleName.Text };
+            var iResult = new IdentityResult();
+            iResult = rManager.Create(nRole);
+            if (iResult.Succeeded)
+            {
+                lblOutput.Text = $"{nRole.Name} role has been created.";
+                txtRoleName.Text = null;
+            }
+            else
+            {
+                lblOutput.Text = iResult.Errors.ToList()[0];
+                lblOutput.ForeColor = System.Drawing.Color.Red;
+            }
+            LoadRoles();
+        }
+
+        protected void btnAssignUser_Click(object sender, EventArgs e)
+        {
+            var user = manager.FindByEmail(TextBox1.Text);
+
+            if (user != null)
+            {
+                foreach (ListItem item in RolesList.Items)
+                {
+                    if (item.Selected)
+                    {
+                        var nRole = rManager.FindByName(item.Text);
+                        if (nRole != null)
+                        {
+                            if (!manager.IsInRole(user.Id, nRole.Name))
+                            {
+                                manager.AddToRole(user.Id, nRole.Name);
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
